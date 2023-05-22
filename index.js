dbac4735f24bebc81e709c98c3aa96c467e40287
gleidsonteixeira <gleidsondeveloper@gmail.com>
2023-05-22 08:41:46 -0300

    update routes

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const app = express();
 const port = 8000;
 const host = `http://localhost:${port}`;
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("so testando")
 });
+
+app.post('/sala', (req, res) => {
+    res.send(JSON.stringify(req.body))
+})
 
 app.listen(port, () => {
     console.log(`Executando no link: ${host}`)
